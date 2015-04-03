328783c27ab469ca93e4d2a9d20e13e3eebaff94
Arnav Gupta <championswimmer@gmail.com>
2015-04-03 17:06:58 +0530

    Initial commit

---
+++ mobile/src/main/java/in/tosc/gadimeter/fare/CurrentFare.java
@@ -0,0 +1,15 @@
+package in.tosc.gadimeter.fare;
+
+/**
+ * Created by championswimmer on 3/4/15.
+ */
+public class CurrentFare {
+    public String city = "";
+    public String operator = "";
+    public int waitingcharge = 0;
+    public int mindist = 0;
+    public int minfare = 0;
+    public int fareperkm = 0;
+    public int bookingfee = 0;
+
+}
